d6e41e9cae8f001b2ff9960011586c6a6ee12ece
fercast711 <fercast7.fc@gmail.com>
2024-08-30 14:16:13 -0600

    PairCount and WordCount completed

--- WordCount/src/main/java/org/example/WordCountMapper.java
+++ WordCount/src/main/java/org/example/WordCountMapper.java
@@ -9,15 +9,37 @@ import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.JobConf;
 public class WordCountMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
     private final static IntWritable one = new IntWritable(1);
     private final Text word = new Text();
+    private String type;
+    @Override
+    public void configure(JobConf job){
+        this.type = job.get("map.task.type");
+    }
+    @Override
     public void map(LongWritable key, Text value,OutputCollector<Text,IntWritable> output, Reporter reporter) throws IOException{
+        if (value == null) {
+            return;
+        }
         String line = value.toString();
         StringTokenizer  tokenizer = new StringTokenizer(line);
-        while (tokenizer.hasMoreTokens()){
-            word.set(tokenizer.nextToken());
-            output.collect(word, one);
+
+        if("WordCount".equals(type)){
+            while (tokenizer.hasMoreTokens()){
+                word.set(tokenizer.nextToken());
+                output.collect(word, one);
+            }
+        } else if("PairCount".equals(type)){
+            String[] words = line.split("\\s+");
+
+            for(int i=0; i < words.length-1; i++){
+                String pair = words[i] + " " + words[i+1];
+                word.set(pair);
+                output.collect(word, one);
+            }
         }
+
     }
 }
