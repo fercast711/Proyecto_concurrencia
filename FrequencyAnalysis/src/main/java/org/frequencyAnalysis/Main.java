84b22cee0dcf27e5b9c4a21fa9ea0340e316c35b
andreaortez <andreaortez11@gmail.com>
2024-09-04 22:29:58 -0600

    arreglos de ortografía (barbaridad tatiana)

--- FrequencyAnalysis/src/main/java/org/frequencyAnalysis/Main.java
+++ FrequencyAnalysis/src/main/java/org/frequencyAnalysis/Main.java
@@ -9,23 +9,23 @@ public class Main {
 
         while(!isvalid){
             System.out.println("1. Preprocesamiento de los datos");
-            System.out.println("2. Analisis de frecuencias");
-            System.out.print("¿Que desea realizar?: ");
+            System.out.println("2. Análisis de frecuencias");
+            System.out.print("¿Qué desea realizar?: ");
             if(scanner.hasNextInt()){
                 isvalid = true;
                 num = scanner.nextInt();
                 if(num != 1 && num != 2){
                     System.out.println(" ");
-                    System.out.println("Solo puede ingresar los numeros 1 o 2");
+                    System.out.println("Solo puede ingresar los números 1 o 2");
                     isvalid = false;
                 }
             } else {
                 System.out.println(" ");
-                System.out.println("El tipo de dato que ingreso no es valido");
+                System.out.println("El tipo de dato que ingresó no es válido");
                 scanner.next();
             }
         }
-        if(num ==1){
+        if(num == 1){
             DataPreprocessor preprocesamiento = new DataPreprocessor();
             preprocesamiento.data_processing();
         }
