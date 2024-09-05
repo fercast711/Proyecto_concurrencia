1dd541c0173413ac8d9c0a6ec7c653f6cc1d08f9
Tatiana-Garcia <tzgarcia_16@unitec.edu>
2024-09-04 21:12:28 -0600

    Agregue stopwords Andrea :)

--- FrequencyAnalysis/src/main/java/org/frequencyAnalysis/DataPreprocessor.java
+++ FrequencyAnalysis/src/main/java/org/frequencyAnalysis/DataPreprocessor.java
@@ -1,4 +1,35 @@
 package org.frequencyAnalysis;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
 public class DataPreprocessor {
+    public void data_processing(){
+        //Preprocesamiento
+        Set<String> stopwords = new HashSet<>();
+
+        String filePath = "src/recursos/StopwordsDictionary.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                stopwords.add(line); // Imprime cada línea del archivo
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // Ejemplo de uso para filtrar stopwords de una línea de texto
+        String line = "this is an example line";
+        String[] words = line.split("\\s+");
+        for (String word : words) {
+            if (!stopwords.contains(word)) {
+                System.out.print(word + " ");
+            }
+        }
+    }
 }
